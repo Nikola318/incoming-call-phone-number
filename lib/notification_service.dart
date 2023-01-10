907346a46eb1b60e2305e935621d56e687161654
Nikola318 <nikola38.jovanovic@gmail.com>
2023-01-10 20:18:48 +0400

    update

--- lib/notification_service.dart
+++ lib/notification_service.dart
@@ -1,3 +1,4 @@
+import 'dart:io' show Platform;
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class NotificationService {
@@ -8,32 +9,60 @@ class NotificationService {
     return _notificationService;
   }
 
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
   NotificationService._internal();
-  
+
   Future<void> init() async {
-  final AndroidInitializationSettings initializationSettingsAndroid =
-      AndroidInitializationSettings('app_icon');
-  
-  final IOSInitializationSettings initializationSettingsIOS =
-      IOSInitializationSettings(
-    requestSoundPermission: false,
-    requestBadgePermission: false,
-    requestAlertPermission: false,
-    onDidReceiveLocalNotification: onDidReceiveLocalNotification,
-  );
-  
+    final AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('app_icon');
+
+    final DarwinInitializationSettings initializationSettingsIOS =
+        DarwinInitializationSettings(
+            requestSoundPermission: false,
+            requestBadgePermission: false,
+            requestAlertPermission: false);
+
     final InitializationSettings initializationSettings =
         InitializationSettings(
-            android: initializationSettingsAndroid, 
-            iOS: initializationSettingsIOS, 
+            android: initializationSettingsAndroid,
+            iOS: initializationSettingsIOS,
             macOS: null);
-  
-   await flutterLocalNotificationsPlugin.initialize(initializationSettings,
-        onSelectNotification: selectNotification);
+
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings);
   }
 
+  Future selectNotification(String payload) async {
+    //Handle notification tapped logic here
+  }
 
-   Future selectNotification(String payload) async {
-      //Handle notification tapped logic here
-   }
+  Future<void> showNotification(String number) async {
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+        AndroidNotificationDetails("channelId", "channelName");
+    const DarwinNotificationDetails iOSPlatformChannelSpecifics =
+        DarwinNotificationDetails();
+    // const DarwinNotificationDetails iOSPlatformChannelSpecifics =
+    //   IOSNotificationDetails(
+    //       presentAlert: bool?,  // Present an alert when the notification is displayed and the application is in the foreground (only from iOS 10 onwards)
+    //       presentBadge: bool?,  // Present the badge number when the notification is displayed and the application is in the foreground (only from iOS 10 onwards)
+    //       presentSound: bool?,  // Play a sound when the notification is displayed and the application is in the foreground (only from iOS 10 onwards)
+    //       sound: String?,  // Specifics the file path to play (only from iOS 10 onwards)
+    //       badgeNumber: int?, // The application's icon badge number  (only from iOS 10 onwards)
+    //       attachments: List<IOSNotificationAttachment>?,
+    //       subtitle: String?, //Secondary description  (only from iOS 10 onwards)
+    //       threadIdentifier: String? (only from iOS 10 onwards)
+    //  );
+
+    const NotificationDetails platformChannelSpecifics = NotificationDetails(
+        android: androidPlatformChannelSpecifics,
+        iOS: iOSPlatformChannelSpecifics);
+
+    await flutterLocalNotificationsPlugin.show(
+        12345,
+        "A Notification From My Application",
+        "This notification was sent using Flutter Local Notifcations Package",
+        platformChannelSpecifics,
+        payload: 'data');
+  }
 }
